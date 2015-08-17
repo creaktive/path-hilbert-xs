c7b1feee0b2c2a7076f8d3f0503ef2056ac6211f
Gonzalo Diethem <gonzalo.diethelm@gmail.com>
2015-08-17 11:48:59 +0200

    Increment version number and document latest changes.

--- lib/Path/Hilbert/XS.pm
+++ lib/Path/Hilbert/XS.pm
@@ -4,7 +4,7 @@ use warnings;
 use XSLoader;
 use parent 'Exporter';
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 our @EXPORT  = qw<d2xy xy2d>;
 
 XSLoader::load( 'Path::Hilbert::XS', $VERSION );
@@ -21,7 +21,7 @@ Path::Hilbert::XS - XS implementation of a Hilbert Path algorithm
 
 =head1 VERSION
 
-0.002
+0.003
 
 =head1 SYNOPSIS
 
